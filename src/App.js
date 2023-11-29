4cd2c68853bac09a4387415dc2f5f0cfcc452b9c
Emmanuel Ajayi <emmanuelajayi@Emmanuels-MacBook-Pro.local>
2023-11-29 14:17:06 +0000

    Lab used to learn intro to React

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [counter, setCounter] = useState(0);             // useState is a function that returns an Array. A value and a function
+
+  const incrementCounter = ()=>{
+    setCounter(counter + 1)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>HELLO WORLD!</h1>
+      <p>Welcome to my amazing React app!</p>
+      <hr />
+      <p>The current value of the counter is {counter}</p>
+      <button onClick={incrementCounter}>Increment Counter</button>
+    </>
   );
 }
 
